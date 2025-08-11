56da6aefc992b5987475ab440067ad53b1c6948c
Bouyahi Mohamed <bouyahi.mohamed.1@gmail.com>
2025-08-11 11:10:21 +0200

    start point for my api-Produict

--- customModule.js
+++
@@ -1,7 +0,0 @@
-function log(message) {
-    console.log(message);
-}
-
-module.exports = { log };
-// This is a custom logging module that exports a log function
-// which can be used to log messages to the console.
